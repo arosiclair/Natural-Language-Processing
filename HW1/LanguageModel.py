b63fa362e883f92679f70f90e2f0300e856e49be
arosiclair <arosiclair@gmail.com>
2016-02-18 23:45:46 -0500

    Implemented Katz backoff, changed corpus preprocessing

--- HW1/LanguageModel.py
+++ HW1/LanguageModel.py
@@ -1,24 +1,27 @@
 class LanguageModel:
 
-  #Constructor
+  #Preprcess the corpus so that it is more easily scanned.
   def __init__(self, corpus):
-    #Instance variable to all lower case corpus
+    #Create instance variable of all lowercase input corpus
     self.corpus = corpus.lower()
     #Split string into sentences delimited by a period
     self.sentences = self.corpus.split(".")
 
     #Split sentences into a list of all words in the corpus, delimitted by a space
+    #Start and end tokens are inserted at the start and end of sentences
     self.words = []
     for sentence in self.sentences:
-      self.words.extend(sentence.split(" "))
+      self.words.append("<s>")
+      self.words.extend(sentence.split())
+      self.words.append("</s>")
 
-    #Count the number of unique words in the corpus for use with smoothing
-    uniqueWords = []
-    self.numUniqueWords = 0
+    #Scan the corpus for its vocabulary
+    self.vocabulary = []
+    self.vocabularySize = 0
     for word in self.words:
-      if word not in uniqueWords:
-        uniqueWords.append(word)
-        self.numUniqueWords += 1
+      if word not in self.vocabulary:
+        self.vocabulary.append(word)
+        self.vocabularySize += 1
 
     #Dictionaries that will save the count for words and bigrams we have already counted
     self.wordCount = {}
@@ -26,10 +29,12 @@ class LanguageModel:
 
   #Returns the joint probability of a sequence of words 
   def getSequenceProb(self, wordList):
-    print wordList
     #Start off with the MLE of the first word in the sequence
     result = self.getMLE(wordList[0])
     print result
+
+    wordList.insert(0, "<s>")
+    wordList.append("</s>")
 
     #Multiply the bigram probabilities of the next successive pairs of words in 
     #the sequence
@@ -41,20 +46,37 @@ class LanguageModel:
 
   #Returns the MLE conditional probability of the bigram "word1 word2"
   def getBigramProb(self, word1, word2):
-    numBigrams = self.bigramCount(word1, word2)
+    numBigrams = self.count(word1, word2)
     return float(numBigrams) / self.count(word1)
 
   #Returns the MLE conditional probability of the bigram "word1 word2" with 
   #Laplace smoothing
   def getSmoothedBigramProb(self, word1, word2):
-    numBigrams = self.bigramCount(word1, word2)
-    return float(numBigrams + 1)) / (self.count(word1) + self.numUniqueWords)
+    numBigrams = self.count(word1, word2)
+    return float(numBigrams + 1) / (self.count(word1) + self.vocabularySize + 1)
 
-  #Returns the Absolute Discounting probability of a bigram
-  def getADProb(self, word1, word2):
+  #Returns the probability of a bigram occuring using Katz Back-off method
+  def katzBigramProb(self, word1, word2):
+    #2 Part function: if the bigram is seen
+    if self.count(word1, word2) > 0:
+      return self.getADProb(word1, word2)
+
+    #If the bigram is unseen
+    elif self.count(word1, word2) == 0:
+      return self.alpha(word1) * self.beta(word2)
     
-    #discount the bigramCount by 0.5
-    return (bigramCount(word1, word2) - 0.5) / self.count(word1)
+  #Returns the Absolute Discounting probability of a bigram
+  def getADProb(self, word1, word2 = None):
+    #Unigram probability
+    if word2 == None:
+      if self.count(word) == 0:
+        return 1.0 / self.vocabularySize
+      else:
+        return (self.count(word) - 0.5) / self.count(word)
+
+    #Bigram probability
+    else:
+      return (self.count(word1, word2) - 0.5) / self.count(word1)
 
 
   #Returns the Maximum Likelihood Estimate for the occurence of a single word
@@ -64,35 +86,54 @@ class LanguageModel:
 
   #Get's the number of occurences for a word in the corpus. Saves it in the 
   #wordCount map for quick access
-  def count(self, word):
-    #Try to immediately retrieve the word count from the dictionary  
-    try:
-      return self.wordCount[word]
-    except KeyError, e:
-      #We haven't counted this word yet so count now
-      num = 0
-      for sample in self.words:
-        if sample == word:
-          num += 1
+  def count(self, word1, word2 = None):
+    #Only count the unigram
+    if word2 == None:
+      #Try to immediately retrieve the word count from the dictionary   
+      try:
+        return self.wordCount[word1]
+      except KeyError, e:
+        #We haven't counted this word yet so count now
+        num = 0
+        for sample in self.words:
+          if sample == word1:
+            num += 1
 
-      #Save count to the dictionary and return
-      self.wordCount[word] = num
-      return num
+        #Save count to the dictionary and return
+        self.wordCount[word1] = num
+        return num
+    #Count the bigram
+    else:
+      #Try to immediately retrieve the bigram count from the dictionary
+      try:
+        return self.bigramCount[(word1, word2)]
+      except KeyError, e:
+        #We haven't counted this bigram yet so count now
+        count = 0
+        prev = self.words[0]
+        for word in self.words:
+          if prev == word1 and word == word2:
+            count += 1
 
-#Get's the number of occurences for a bigram in the corpus. Saves it in the
-#bigramCount map for quick access
-def bigramCount(self, word1, word2):
-  try:
-    return self.bigramCount[(word1, word2)]
-  except KeyError, e:
-    #We haven't counted this bigram yet so count now
-    count = 0
-    prev = self.words[0]
-    for word in self.words:
-      if prev == word1 and word == word2:
-        count += 1
+          prev = word
 
-      prev = word
+        self.bigramCount[(word1, word2)] = count
+        return count
 
-    self.bigramCount[(word1, word2)] = count
-    return count
+  #Finds the amount of probability reserved for unseen bigrams beginning with word1
+  def alpha(self, word1):
+    remainingProb = 1.0
+
+    for word in self.vocabulary:
+      remainingProb -= self.getADProb(word1, word)
+
+    return remainingProb
+
+  def beta(self, word2):
+    numerator = getADProb(word2)
+    denominator = 0
+
+    for word in self.vocabulary:
+      denominator += getADProb(word)
+
+    return numerator / denominator
