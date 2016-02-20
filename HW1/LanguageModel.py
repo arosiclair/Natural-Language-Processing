df0271b04e559aa0a3e12d0f83c7abc60ca09efc
arosiclair <arosiclair@gmail.com>
2016-02-20 15:21:44 -0500

    Implemented language model and top bigrams builder

--- HW1/LanguageModel.py
+++ HW1/LanguageModel.py
@@ -11,9 +11,11 @@ class LanguageModel:
     #Start and end tokens are inserted at the start and end of sentences
     self.words = []
     for sentence in self.sentences:
-      self.words.append("<s>")
-      self.words.extend(sentence.split())
-      self.words.append("</s>")
+
+      if(sentence.split() != ""):
+        self.words.append("<s>")
+        self.words.extend(sentence.split())
+        self.words.append("</s>")
 
     #Scan the corpus for its vocabulary
     self.vocabulary = []
@@ -66,10 +68,10 @@ class LanguageModel:
   def getADProb(self, word1, word2 = None):
     #Unigram probability
     if word2 == None:
-      if self.count(word) == 0:
+      if self.count(word1) == 0:
         return 1.0 / self.vocabularySize
       else:
-        return (self.count(word) - 0.5) / self.count(word)
+        return (self.count(word1) - 0.5) / len(self.words)
 
     #Bigram probability
     else:
@@ -78,8 +80,10 @@ class LanguageModel:
 
   #Returns the Maximum Likelihood Estimate for the occurence of a single word
   def getMLE(self, word):
-
     return float(self.count(word)) / len(self.words)
+
+  def getSmoothedUnigramProb(self, word):
+    return float(self.count(word) + 1) / (len(self.words) + self.vocabularySize)
 
   #Get's the number of occurences for a word in the corpus. Saves it in the 
   #wordCount map for quick access
@@ -127,10 +131,179 @@ class LanguageModel:
     return remainingProb
 
   def beta(self, word2):
-    numerator = getADProb(word2)
+    numerator = self.getADProb(word2)
     denominator = 0
 
     for word in self.vocabulary:
-      denominator += getADProb(word)
+      denominator += self.getADProb(word)
 
     return numerator / denominator
+
+  def buildLanguageModelFile(self):
+    bigramsOutput = open("LanguageModel.txt", 'w')
+
+    #Iterate through each succesive bigram in the corpus, keeping track of counts 
+    #in the bigrams dictionary
+    prev = self.words[0]
+    for i in xrange(1, len(self.words)):
+      word = self.words[i]
+      try:
+        num = self.bigramCount[(prev, word)]
+        self.bigramCount[(prev, word)] = num + 1
+      except KeyError, e:
+        self.bigramCount[(prev, word)] = 1
+
+      prev = word
+
+    #File header
+    bigramsOutput.write("-" * 85 + "\n")
+    bigramsOutput.write("%49s" % "LANGUAGE MODEL" + "\n")
+    bigramsOutput.write("-" * 85 + "\n")
+    bigramsOutput.write("%-40s%15s%15s%15s" % ("Bigram", "Frequency", "MLE", "Katz") + "\n")
+    bigramsOutput.write("-" * 85 + "\n")
+
+    #Print all bigrams seen in training along wit their frequency, MLE and Katz
+    #estimations
+    for bigram in self.bigramCount.keys():
+      bigramsOutput.write("%-20s%-20s%15s" % (bigram[0], bigram[1], self.bigramCount[bigram]))
+      bigramsOutput.write("%15.10f%15.10f\n" % (self.getBigramProb(bigram[0], bigram[1]),
+                                        self.katzBigramProb(bigram[0], bigram[1])))
+    bigramsOutput.write("-" * 85 + "\n")
+
+    #Header for unigram frequencies
+    bigramsOutput.write("\n%-20s%9s\n" % ("Unigram", "Frequency"))
+    bigramsOutput.write("-" * 85 + "\n")
+
+    #Print all unigrams seen in training along with their frequency
+    for word in self.vocabulary:
+      bigramsOutput.write("%-20s%9s\n" % (word, self.count(word)))
+
+  def buildTopBigramsFile(self):
+    output = open("TopBigrams.txt", 'w')
+
+    self.writeTopMLE(output)
+    self.writeTopLaplace(output)
+    self.writeTopAD(output)
+    self.writeTopKatz(output)
+
+
+  def writeTopMLE(self, output):
+    #List of top 20 MLE bigrams
+    top20MLE = []
+
+    for bigram in self.bigramCount.keys():
+      #The joint MLE probability
+      prob = self.getBigramProb(bigram[1], bigram[0]) * self.getMLE(bigram[0])
+
+      #Insert bigrams and probability as a 3 element list into the top20MLE list
+      for i in xrange(0, 20):
+        if i == len(top20MLE) and i < 20:
+          top20MLE.append([bigram[0], bigram[1], prob])
+        if prob > top20MLE[i][2]:
+          top20MLE.insert(i, [bigram[0], bigram[1], prob])
+          if len(top20MLE) == 21:
+            top20MLE.remove(top20MLE[20])
+          break
+
+    #Top 20 MLE Header
+    output.write("-" * 60 + "\n")
+    output.write("%39s\n" % "TOP 20 MLE BIGRAMS") 
+    output.write("-" * 60 + "\n")
+    output.write("%-40s%20s\n" % ("Bigram", "Joint Probability")) 
+    output.write("-" * 60 + "\n")
+
+    #Print the top 20
+    i = 1
+    for bigram in top20MLE:
+      output.write("%-3d%-17s%-20s%20.10f\n" % (i, bigram[0], bigram[1], bigram[2])) 
+      i += 1
+
+  def writeTopLaplace(self, output):
+    top20Laplace = []
+    #Top 20 Laplace Smoothing
+    for bigram in self.bigramCount.keys():
+      #The joint MLE probability
+      prob = self.getSmoothedBigramProb(bigram[1], bigram[0]) * self.getSmoothedUnigramProb(bigram[0])
+
+      #Insert bigrams and probability as a 3 element list into the top20Laplace list
+      for i in xrange(0, 20):
+        if i == len(top20Laplace) and i < 20:
+          top20Laplace.append([bigram[0], bigram[1], prob])
+        if prob > top20Laplace[i][2]:
+          top20Laplace.insert(i, [bigram[0], bigram[1], prob])
+          if len(top20Laplace) == 21:
+            top20Laplace.remove(top20Laplace[20])
+          break
+
+    #Top 20 MLE Header
+    output.write("-" * 60 + "\n")
+    output.write("%41s\n" % "TOP 20 LAPLACE BIGRAMS") 
+    output.write("-" * 60 + "\n")
+    output.write("%-40s%20s\n" % ("Bigram", "Joint Probability")) 
+    output.write("-" * 60 + "\n")
+
+    #Print the top 20
+    i = 1
+    for bigram in top20Laplace:
+      output.write("%-3d%-17s%-20s%20.10f\n" % (i, bigram[0], bigram[1], bigram[2])) 
+      i += 1
+
+  def writeTopAD(self, output):
+    top20AD = []
+    #Top 20 Absolute Discounting
+    for bigram in self.bigramCount.keys():
+      #The joint probability
+      prob = self.getADProb(bigram[1], bigram[0]) * self.getADProb(bigram[0])
+
+      #Insert bigrams and probability as a 3 element list into the top20AD list
+      for i in xrange(0, 20):
+        if i == len(top20AD) and i < 20:
+          top20AD.append([bigram[0], bigram[1], prob])
+        if prob > top20AD[i][2]:
+          top20AD.insert(i, [bigram[0], bigram[1], prob])
+          if len(top20AD) == 21:
+            top20AD.remove(top20AD[20])
+          break
+
+    #Top 20 MLE Header
+    output.write("-" * 60 + "\n")
+    output.write("%47s\n" % "TOP 20 ABSOLUTE DISCOUNTED BIGRAMS")
+    output.write("-" * 60 + "\n")
+    output.write("%-40s%20s\n" % ("Bigram", "Joint Probability")) 
+    output.write("-" * 60 + "\n")
+
+    #Print the top 20
+    i = 1
+    for bigram in top20AD:
+      output.write("%-3d%-17s%-20s%20.10f\n" % (i, bigram[1], bigram[0], bigram[2]))
+      i += 1
+
+  def writeTopKatz(self, output):
+    top20Katz = []
+    #Top 20 Absolute Discounting
+    for bigram in self.bigramCount.keys():
+      #The joint probability
+      prob = self.getADProb(bigram[1], bigram[0]) * self.getADProb(bigram[0])
+
+      #Insert bigrams and probability as a 3 element list into the top20Katz list
+      for i in xrange(0, 20):
+        if i == len(top20Katz) and i < 20:
+          top20Katz.append([bigram[0], bigram[1], prob])
+        if prob > top20Katz[i][2]:
+          top20Katz.insert(i, [bigram[0], bigram[1], prob])
+          if len(top20Katz) == 21:
+            top20Katz.remove(top20Katz[20])
+          break
+
+    #Top 20 MLE Header
+    output.write("-" * 60 + "\n")
+    output.write("%44s\n" % "TOP 20 KATZ BACK-OFF BIGRAMS") 
+    output.write("-" * 60 + "\n")
+    output.write("%-40s%20s\n" % ("Bigram", "Joint Probability")) 
+    output.write("-" * 60 + "\n")
+
+    #Print the top 20
+    i = 1
+    for bigram in top20Katz:
+      output.write("%-3d%-17s%-20s%20.10f\n" % (i, bigram[0], bigram[1], bigram[2]))
+      i += 1
