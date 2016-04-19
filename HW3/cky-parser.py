64c047114ce1e1da93f74290143861f596bd646c
arosiclair <arosiclair@gmail.com>
2016-04-18 21:07:39 -0400

    reimplemented CKY Parser

--- HW3/cky-parser.py
+++ HW3/cky-parser.py
@@ -1,103 +1,218 @@
-#CKY Parser
-
+# CKY Parser
+from __future__ import division
 from pickle import load
 from math import log
+from tree import Tree, Node
 
-#Load serialized objects
+
+# Load serialized objects
 def load_obj(name):
     with open(name + '.pkl', 'rb') as f:
         return load(f)
 
-#Parsing algorithm
-def parse(line):
-	words = line.split(" ")	#the sequence of words
 
-	score = initArray(words)
-	back = initArray(words)
+# Parsing algorithm
+def parse(inputLine):
+    inputLine = inputLine.strip()
+    words = inputLine.split(" ")  # the sequence of words
 
-	for i in xrange(0, len(words)):
-		begin = i
-		end = i + 1
+    score = initArray(words)
+    back = initArray(words)
 
-		#Add the POS tags for this word
-		for j in xrange(0, len(nonterminals)):
-			prob = 1 / (pow(len(nonterminals) - 1, 2) + len(terminals))
-			try:
-				nt = nonterminals[j]
-				prob = pcfg[(nt, words[i])]
-				score[begin][end][j] = prob
-			except KeyError, e:
-				continue
+    # Iterate over each column in the table
+    for j in xrange(0, len(words)):
 
-		#Add all unary rules that match
-		addUnary(score, back, begin, end)
+        # Add the POS tag probabilities to the bottom entry of this column
+        for i in xrange(0, len(nonterminals)):
+            prob = -1
+            try:
+                nt = nonterminals[i]
+                prob = pcfg[(nt, words[j])]
+            except KeyError, e:
+                prob = 1 / (pow(len(nonterminals) - 1, 2) + len(terminals))
 
-	for span in xrange(2, len(words)):
-		for begin in xrange(0, len(words) - span):
-			end = begin + span
-			for split in xrange(begin + 1, end - 1):
-				for triple in getTriples():
-					aindex = nonterminals.index(triple[0])
-					bindex = nonterminals.index(triple[1])
-					cindex = nonterminals.index(triple[2])
-					tripleProb = None
-					try:
-						tripleProb = pcfg[triple]
-					except KeyError, e:
-						continue
+            score[j][j][i] = log(prob)
 
-					prob = log(score[begin][split][bindex]) + log(scorescore[begin][split][cindex]) + log(tripleProb)
-					if(prob > score[begin][end][aindex]):
-						score[begin][end][aindex] = prob
-						back[begin][end][aindex] = tuple([split, bindex, cindex])
+        # Iterate over the rest of the rows above in the column.
+        for i in xrange(j - 1, -1, -1):
+            #Try every splitting index for this entry
+            for k in xrange(i, j):
+                for rule in pcfg:
+                    if len(rule) == 2:
+                        continue
+                    aindex = nonterminals.index(rule[0])
+                    bindex = nonterminals.index(rule[1])
+                    cindex = nonterminals.index(rule[2])
+                    if score[i][k][bindex] != 0 and score[k + 1][j][cindex] != 0:
+                        ruleProb = -1
+                        try:
+                            ruleProb = pcfg[rule]
+                        except KeyError, e:
+                            ruleProb = 1 / (pow(len(nonterminals) - 1, 2) + len(terminals))
+                        prob = log(ruleProb) + score[i][k][bindex] + score[k + 1][j][cindex]
+                        if prob > score[i][j][aindex] or score[i][j][aindex] == 0:
+                            score[i][j][aindex] = prob
+                            back[i][j][aindex] = (k, bindex, cindex)
 
-				#Add all unary rules that match		
-				addUnary(score, back, begin, end)
+    #printArray(score)
+
+    return buildTree(score, back, words, len(words))
+
+
+def buildTree(score, back, words, wordsLength):
+    # Create the root tree
+    rootTree = Tree(None)
+
+    # Find the highest probability nonterminal to use for the root node
+    maxIndex = getMax(score[0][wordsLength - 1])
+
+    # Get the backpointer corresponding to this nonterminal
+    triple = back[0][wordsLength - 1][maxIndex]
+    leftChild = Node(nonterminals[triple[1]], [])
+    rightChild = Node(nonterminals[triple[2]], [])
+
+    # Create the root node and attach it's children
+    rootNode = Node(nonterminals[maxIndex], [leftChild, rightChild])
+    rootTree.root = rootNode
+
+    begin = 0
+    end = wordsLength - 1
+    split = triple[0]  # The splitting index
+
+    # Start by pushing the root's children onto the stack
+    # The subsequence that each child represents is also pushed with them.
+    stack = []
+    stack.append([rightChild, split + 1, end])
+    stack.append([leftChild, begin, split])
+
+    # Continue processing sub trees on the stack
+    while len(stack) > 0:
+        #print "STACK LENGTH:", len(stack)
+        nodeList = stack.pop()
+        parent = nodeList[0]
+        ntIndex = nonterminals.index(parent.label)
+        begin = nodeList[1]
+        end = nodeList[2]
+
+        # If this is a subsequence of multipe words, get the left and right constituents
+        if (end - begin) > 0:
+            # Get the left and right constituents of the subsequence [i:j]
+            triple = back[begin][end][ntIndex]
+            leftChild = Node(nonterminals[triple[1]], [])
+            rightChild = Node(nonterminals[triple[2]], [])
+            split = triple[0]
+
+            # Attach these new children to the parent node
+            parent.append_child(leftChild)
+            parent.append_child(rightChild)
+
+            # Push these new children onto the stack
+            stack.append([rightChild, split + 1, end])
+            stack.append([leftChild, begin, split])
+
+        # Otherwise this is a subsequence of 1 word, attach the word as a child
+        # to the parent, which is a POS tag
+        else:
+            terminal = Node(words[begin], [])
+            parent.append_child(terminal)
+    
+    return rootTree
+
+def outputTree(node):
+
+    if len(node.children) == 0:
+        return node.label
+    elif len(node.children) == 1:
+        return node.label + "(" + outputTree(node.children[0]) + ") "
+    else:
+        return node.label + "(" + outputTree(node.children[0]) + outputTree(node.children[1]) + ") "
+
+def printArray(array):
+    for i in xrange(0, len(array)):
+        for j in xrange(0, len(array[i])):
+            print "\nFOR ENTRY [%d][%d]:" % (i, j)
+            for k in xrange(0, len(array[i][j])):
+                print array[i][j][k], ",",
+
+
+# Get the index of the entry in this list with the max probability
+def getMax(list):
+    maxIndex = 0
+    maxProb = 0
+    for i in xrange(0, len(list)):
+        if list[i] > maxProb:
+            maxProb = list[i]
+            maxIndex = i
+
+    return maxIndex
+
 
 def initArray(words):
-	result = [0] * (len(words) + 1)
-	for i in xrange(0, len(result)):
-		result[i] = [0] * (len(words) + 1)
+    result = [0] * (len(words))
+    for i in xrange(0, len(result)):
+        result[i] = [0] * (len(words))
 
-	for i in xrange(0, len(result)):
-		for j in xrange(0, len(result[i])):
-			result[i][j] = [0] * len(nonterminals)
+    for i in xrange(0, len(result)):
+        for j in xrange(0, len(result[i])):
+            result[i][j] = [0] * len(nonterminals)
 
-	return result
+    return result
+
 
 def addUnary(score, back, begin, end):
-	added = True
-	while added:
-		for nt in nonterminals:
-			for term in terminals:
-				ruleProb = 1 / (pow(len(nonterminals) - 1, 2) + len(terminals))
-				try:
-					ruleProb = pcfg[(nt, term)]
-				except KeyError, e:
-					pass
-				#TODO: This probability doesn't make sense.
-				prob = log(ruleProb) + log(score[begin][end][len(nonterminals) + terminals.index(term)])
+    added = True
+    while added:
+        added = False
+        for nt in nonterminals:
+            for term in terminals:
+                aindex = nonterminals.index(nt)
+                bindex = len(nonterminals) + terminals.index(term)
+                ruleProb = 1 / (pow(len(nonterminals) - 1, 2) + len(terminals))
+                try:
+                    ruleProb = pcfg[(nt, term)]
+                except KeyError, e:
+                    pass
+                # TODO: This probability doesn't make sense.
+                prob = log(ruleProb) + log(score[begin][end][bindex])
+                if prob > score[begin][end][aindex]:
+                    score[begin][end][aindex] = prob
+                    back[begin][end][aindex] = (-1, bindex, None)
+                    added = True
+
 
 def getTriples():
-	triples = []
-	for A in nonterminals:
-		for B in nonterminals:
-			for C in nonterminals:
-				triples.append(tuple([A, B, C]))
+    result = []
+    for A in nonterminals:
+        for B in nonterminals:
+            for C in nonterminals:
+                result.append(tuple([A, B, C]))
 
-	return triples
+    return result
 
-#--- BEGIN SCRIPT ---
-#Open and read each line of the test sequences
+
+# --- BEGIN SCRIPT ---
+# Open and read each line of the test sequences
 testFile = open("test.txt")
 testLines = testFile.readlines()
 testFile.close()
 
-#Load the PCFG
+outputFile = open("output.trees", "w")
+
+# Load the PCFG
 pcfg = load_obj("pcfg")
-#Load our set of nonterminals from training
+# Load our set of nonterminals from training
 nonterminals = load_obj("nonterminals")
 terminals = load_obj("terminals")
 
+triples = getTriples()
+
+parsedTrees = []
 for line in testLines:
-	parse(line)
+    parsedTrees.append(parse(line))
+    #print "LINE PARSED"
+
+for tree in parsedTrees:
+    output = outputTree(tree.root) + "\n"
+    outputFile.write(output)
+
+print "Output tree file produced: 'output.trees'"
--- HW3/pcfg-builder.py
+++ HW3/pcfg-builder.py
@@ -1,164 +1,177 @@
-#Probability CFG builder
+# Probability CFG builder
 from __future__ import division
 import tree
 from pickle import dump, HIGHEST_PROTOCOL
 
-#Generate list of unique nonterminals from the training trees
-def getNonterminals():
-	for i in xrange(0, len(trainInput)):
-		begin = None
-		end = None
-		if trainInput[i] == '(':
-			end = i;
-			for j in xrange(i - 1, 0, -1):
-				if trainInput[j] == ' ' or trainInput[j] == '\n' or trainInput[j] == '(':
-					begin = j + 1
-					break
 
-			nt = trainInput[begin : end]
-			if nt not in nonterminals:
-				nonterminals.append(trainInput[begin : end])
+# Generate list of unique nonterminals from the training trees
+def getNonterminals():
+    for i in xrange(0, len(trainInput)):
+        begin = None
+        end = None
+        if trainInput[i] == '(':
+            end = i;
+            for j in xrange(i - 1, 0, -1):
+                if trainInput[j] == ' ' or trainInput[j] == '\n' or trainInput[j] == '(':
+                    begin = j + 1
+                    break
+
+            nt = trainInput[begin: end]
+            if nt not in nonterminals:
+                nonterminals.append(trainInput[begin: end])
+
 
 def getTerminals():
-	for tree in trees:
-		for leaf in tree.leaves():
-			if leaf.label not in terminals:
-				terminals.append(leaf.label)
+    for tree in trees:
+        for leaf in tree.leaves():
+            if leaf.label not in terminals:
+                terminals.append(leaf.label)
 
-#Iterate over the training examples and generate a tree for each line
+
+# Iterate over the training examples and generate a tree for each line
 def generateTrees():
-	for line in trainLines:
-		if line.strip() == 0:
-			continue
+    for line in trainLines:
+        if line.strip() == 0:
+            continue
 
-		parsedTree = tree.Tree.from_str(line)
-		trees.append(parsedTree)
+        parsedTree = tree.Tree.from_str(line)
+        trees.append(parsedTree)
+
 
 def countRules(tree):
-	global ruleCounts
-	stack = []
-	stack.append(tree.root)
+    global ruleCounts
+    stack = []
+    stack.append(tree.root)
 
-	while(len(stack) > 0):
-		parent = stack.pop()
+    while len(stack) > 0:
+        parent = stack.pop()
 
-		#This is a leaf node with no expansion, skip it
-		if len(parent.children) == 0:
-			continue
+        # This is a leaf node with no expansion, skip it
+        if len(parent.children) == 0:
+            continue
 
-		stack.extend(parent.children)
-		expansions = None #The sub dictionary with RHS expansions
-		#Try getting a reference to the expansions dictionary for this nonterminal
-		try:
-			expansions = ruleCounts[parent.label]
-		#This is the first time we're seeing this nonterminal, create a new 
-		#expansions dict for it
-		except KeyError, e:
-			ruleCounts[parent.label] = {}
-			expansions = ruleCounts[parent.label]
-		
-		childLabels = []
-		for child in parent.children:
-			childLabels.append(child.label)
-		#Increment the number of occurences for this nonterminal -> RHS expansion
-		try:
-			expansions[tuple(childLabels)] += 1
-		except KeyError, e:
-			expansions[tuple(childLabels)] = 1
+        stack.extend(parent.children)
+        expansions = None  # The sub dictionary with RHS expansions
+        # Try getting a reference to the expansions dictionary for this nonterminal
+        try:
+            expansions = ruleCounts[parent.label]
+        # This is the first time we're seeing this nonterminal, create a new
+        # expansions dict for it
+        except KeyError, e:
+            ruleCounts[parent.label] = {}
+            expansions = ruleCounts[parent.label]
+
+        childLabels = []
+        for child in parent.children:
+            childLabels.append(child.label)
+        # Increment the number of occurences for this nonterminal -> RHS expansion
+        try:
+            expansions[tuple(childLabels)] += 1
+        except KeyError, e:
+            expansions[tuple(childLabels)] = 1
+
 
 def generatePCFG():
-	global pcfg
+    global pcfg
 
-	for nonterminal in ruleCounts:
-		total = 0
-		for expansion in ruleCounts[nonterminal]:
-			total += ruleCounts[nonterminal][expansion]
+    for nonterminal in ruleCounts:
+        total = 0
+        for expansion in ruleCounts[nonterminal]:
+            total += ruleCounts[nonterminal][expansion]
 
-		for expansion in ruleCounts[nonterminal]:
-			prob = (ruleCounts[nonterminal][expansion] + 1) / (pow(len(nonterminals) - 1, 2) + len(terminals))
-			key = []
-			key.append(nonterminal)
-			key.extend(expansion)
-			pcfg[tuple(key)] = prob
+        for expansion in ruleCounts[nonterminal]:
+            prob = (ruleCounts[nonterminal][expansion] + 1) / (pow(len(nonterminals) - 1, 2) + len(terminals))
+            key = []
+            key.append(nonterminal)
+            for nt in expansion:
+                key.append(nt)
+            pcfg[tuple(key)] = prob
+
 
 def printTop10Rules():
-	top10 = []
-	for lhs in ruleCounts:
-		expansions = ruleCounts[lhs]
-		for expansion in expansions:
-			if len(expansion) < 2:
-				continue
-			for i in xrange(0, len(top10)):
-				if expansions[expansion] > top10[i][-1]:
-					entry = []
-					entry.append(lhs)
-					entry.extend(expansion)
-					entry.append(expansions[expansion])
-					top10.insert(i, entry)
-					break
-			
-			if len(top10) < 10:
-				entry = []
-				entry.append(lhs)
-				entry.extend(expansion)
-				entry.append(expansions[expansion])
-				top10.append(entry)
-				break
+    top10 = []
+    for lhs in ruleCounts:
+        expansions = ruleCounts[lhs]
+        for expansion in expansions:
+            if len(expansion) < 2:
+                continue
+            for i in xrange(0, len(top10)):
+                if expansions[expansion] > top10[i][-1]:
+                    entry = []
+                    entry.append(lhs)
+                    entry.extend(expansion)
+                    entry.append(expansions[expansion])
+                    top10.insert(i, entry)
+                    break
 
-			if len(top10) == 11:
-				top10.pop()
+            if len(top10) < 10:
+                entry = []
+                entry.append(lhs)
+                entry.extend(expansion)
+                entry.append(expansions[expansion])
+                top10.append(entry)
+                break
 
-	for entry in top10:
-		print "%-5s -> %-7s, %-10s| Frequency: %5d" % (entry[0], entry[1], entry[2], entry[3])
+            if len(top10) == 11:
+                top10.pop()
+
+    for entry in top10:
+        print "%-5s -> %-7s, %-10s| Frequency: %5d" % (entry[0], entry[1], entry[2], entry[3])
+
 
 def printRuleCounts():
-	for nonterminal in ruleCounts:
-		for rhs in ruleCounts[nonterminal]:
-			print "%-10s -> %-15s Prob: %f" % (nonterminal, rhs, ruleCounts[nonterminal][rhs])
+    for nonterminal in ruleCounts:
+        for rhs in ruleCounts[nonterminal]:
+            print "%-10s -> %-15s Prob: %f" % (nonterminal, rhs, ruleCounts[nonterminal][rhs])
+
 
 def printPCFG():
-	for rule in pcfg:
-		print "%-10s -> %-25s Prob: %.10f" % (rule[0], rule[1:], pcfg[rule])
+    for rule in pcfg:
+        print "%-10s -> %-25s Prob: %.10f" % (rule[0], rule[1:], pcfg[rule])
+
 
 def save_obj(obj, name):
     with open(name + '.pkl', 'wb') as f:
         dump(obj, f, HIGHEST_PROTOCOL)
 
-#--- BEGIN SCRIPT ---
 
-#Open the training file and get a list of lines
+# --- BEGIN SCRIPT ---
+
+# Open the training file and get a list of lines
 trainFile = open("train.trees")
 trainLines = trainFile.readlines()
 trainFile.seek(0)
 trainInput = trainFile.read()
 trainFile.close()
 
-#A list of unique nonterminals from training
+# A list of unique nonterminals from training
 nonterminals = []
 getNonterminals()
 save_obj(nonterminals, "nonterminals")
 
-trees = []	#A list of Tree objects from training
+trees = []  # A list of Tree objects from training
 generateTrees()
 
-#A list of unique terminals (words) in training
+# A list of unique terminals (words) in training
 terminals = []
 getTerminals()
 save_obj(terminals, "terminals")
 
-#A dictionary with key values: <nonterminal label>, and values: <dictionary>
-#The sub-dictionary will have the right hand side of the rules (ie: (NN, VB)) as a keys
-#and the count of nonterminal -> rhs in training.
-ruleCounts = {} 
+# A dictionary with key values: <nonterminal label>, and values: <dictionary>
+# The sub-dictionary will have the right hand side of the rules (ie: (NN, VB)) as a keys
+# and the count of nonterminal -> rhs in training.
+ruleCounts = {}
 
-#Iterate over the generated parse trees and count the number of occurences for
-#each expansion
+# Iterate over the generated parse trees and count the number of occurences for
+# each expansion
 for tree in trees:
-	countRules(tree)
+    countRules(tree)
 
-#The Probability Context Free Grammar. Key = ([nonterminal, expansion...])
-#Value = laplace smoothed probability of using that nonterminal and expansion
+# The Probability Context Free Grammar. Key = ([nonterminal, expansion...])
+# Value = laplace smoothed probability of using that nonterminal and expansion
 pcfg = {}
 generatePCFG()
 save_obj(pcfg, "pcfg")
-#printTop10Rules()
+# printTop10Rules()
+
+print "PCFG produced: 'pcfg.pkl'"
